3ef12ae4ec34af17a5464bc36b42828eea47060e
William Hubenschmidt <whubenschmidt@partechgss.com>
2023-03-17 01:41:29 -0400

    renders a react component on port 8000

---
+++ resources/js/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Home() {
+    const heading = "Laravel 10 Vite with React JS";
+    return <div>{heading}</div>;
+}
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,7 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+ReactDOM.createRoot(document.getElementById("app")).render(<Home />);
